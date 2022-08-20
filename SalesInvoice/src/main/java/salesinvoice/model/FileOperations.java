88d5f9ebbb0fc4708b81eb1ff9646f7d76503909
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2022-08-20 03:09:26 +0200

    Create FileOperations.java

---
+++ SalesInvoice/src/main/java/salesinvoice/model/FileOperations.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salesinvoice.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DELL
+ */
+public class FileOperations {
+    //Load Files
+    public ArrayList<InvoiceHeader> readFile()
+    {
+        return null;
+    }
+    //Save Data
+    public void writeFile(ArrayList<InvoiceHeader> item)
+    {
+        
+    }
+}
