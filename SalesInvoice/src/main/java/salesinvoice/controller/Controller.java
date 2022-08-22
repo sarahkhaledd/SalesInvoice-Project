e4596ac67e4000642869a67aea7a93cb23ba1823
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2022-08-22 04:37:58 +0200

    Update Controller.java

--- SalesInvoice/src/main/java/salesinvoice/controller/Controller.java
+++ SalesInvoice/src/main/java/salesinvoice/controller/Controller.java
@@ -11,12 +11,10 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import salesinvoice.model.FileOperations;
 import salesinvoice.model.InvoiceHeader;
+import salesinvoice.model.InvoiceHeaderTable;
+import salesinvoice.model.InvoiceLineTable;
 import salesinvoice.view.JFrame;
 
-/**
- *
- * @author DELL
- */
 public class Controller implements ActionListener , ListSelectionListener
 { 
     JFrame jf;
@@ -25,6 +23,7 @@ public class Controller implements ActionListener , ListSelectionListener
     }
     private FileOperations file = new FileOperations(jf) ; 
     private ArrayList<InvoiceHeader> invoiceHeaderArray;
+    private InvoiceHeader header ;
 
     public void actionPerformed(ActionEvent e)
     {
@@ -33,11 +32,14 @@ public class Controller implements ActionListener , ListSelectionListener
         {
             case "Load File":
                invoiceHeaderArray = file.readFile();
+               jf.setInvoiceHeaderArray(invoiceHeaderArray);
+               jf.getjTable1().setModel(new InvoiceHeaderTable(invoiceHeaderArray));
+
                 break;
             case "Save Data":
                 file.writeFile(null);
                 break;
-            case "Create new invoice":
+            case "Create New Invoice":
                 createNewInvoice();
                 break;
             case "Delete invoice":
@@ -73,5 +75,15 @@ public class Controller implements ActionListener , ListSelectionListener
 
     @Override
     public void valueChanged(ListSelectionEvent e) {
+        int numberOfRow = jf.getjTable1().getSelectedRow();
+        if(numberOfRow >=0)
+        {
+            header = jf.getInvoiceHeaderArray().get(numberOfRow);
+            jf.getjLabel5().setText(""+header.getNumber());
+            jf.getjLabel6().setText(""+header.getDate());
+            jf.getjLabel7().setText(""+header.getCustomer());
+            jf.getjLabel8().setText(""+header.getTotal());  
+            jf.getjTable2().setModel(new InvoiceLineTable(header.getItem()));
+        }
     }
 }
