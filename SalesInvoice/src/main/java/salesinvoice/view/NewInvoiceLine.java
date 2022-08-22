013394767d334e5fed61bd8109a48b3670b91c30
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2022-08-22 04:45:06 +0200

    Create NewInvoiceLine.java

---
+++ SalesInvoice/src/main/java/salesinvoice/view/NewInvoiceLine.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salesinvoice.view;
+
+import javax.swing.JDialog;
+
+public class NewInvoiceLine extends JDialog {
+    
+}
