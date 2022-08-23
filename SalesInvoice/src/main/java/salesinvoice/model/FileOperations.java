999c4659f1e2d13c570eac1cb9320b9095f23ba1
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2022-08-23 02:15:07 +0200

    Update FileOperations.java

--- SalesInvoice/src/main/java/salesinvoice/model/FileOperations.java
+++ SalesInvoice/src/main/java/salesinvoice/model/FileOperations.java
@@ -5,10 +5,12 @@
 package salesinvoice.model;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -87,8 +89,8 @@ public class FileOperations {
                    int number = Integer.parseInt(num);
                    double price = Double.parseDouble(itemPrice);
                    int c = Integer.parseInt(count);
-                   InvoiceHeader header= frame.getNumberOfInvoice(number);
-                   il= new InvoiceLine(header.getNumber(),itemName,price,c);  
+                   // header= frame.getNumberOfInvoice(number);
+                   il= new InvoiceLine(number,itemName,price,c);  
                    invoiceLineArray.add(il);  
                    line = reader.readLine();
 		}
@@ -119,37 +121,94 @@ public class FileOperations {
         return invoiceHeaderArray;
     }
     //Save Data
-    public void writeFile(ArrayList<InvoiceHeader> item)
+    public void writeFile(ArrayList<InvoiceHeader> item) throws IOException
     {
+        FileWriter fileLine ;
+        BufferedWriter bufferLineFile = null ;
+        FileWriter fileHeader ;
+        BufferedWriter bufferHeaderFile =null;
        JFileChooser jF = new JFileChooser(); 
         if (jF.showSaveDialog(jF) == JFileChooser.APPROVE_OPTION) {
-            String pathFile = jF.getSelectedFile().getPath();
-            FileOutputStream fos = null;
-            String x;
+            String pathFileHeader = jF.getSelectedFile().getPath();
            try {
-               fos = new FileOutputStream(pathFile);
+               fileHeader = new FileWriter(pathFileHeader, true);
+               bufferHeaderFile = new BufferedWriter(fileHeader);
+
            } catch (FileNotFoundException ex) {
                Logger.getLogger(FileOperations.class.getName()).log(Level.SEVERE, null, ex);
            }
-           // byte[]array =ta.getText().getByte() ;
-            //fos.write(array);
-           finally{
-                try {
-                    fos.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(FileOperations.class.getName()).log(Level.SEVERE, null, ex);
-                }
+        } 
+        jF = new JFileChooser(); 
+        if (jF.showSaveDialog(jF) == JFileChooser.APPROVE_OPTION) {
+            String pathFileLine = jF.getSelectedFile().getPath();
+            
+           try {
+               fileLine = new FileWriter(pathFileLine, true);
+               bufferLineFile = new BufferedWriter(fileLine);
+
+           } catch (FileNotFoundException ex) {
+               Logger.getLogger(FileOperations.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        } 
+        String dataOfHeader = "";
+        String dataOfLine = "";
+        for (int i = 0; i < item.size(); i++) {
+            if(item.size()-1!=i)
+            {
+                dataOfHeader+=item.get(i).getNumber()+","+item.get(i).getDate()+","+item.get(i).getCustomer()+'\n';
+                for (int j = 0; j < item.get(i).getItem().size(); j++) {
+                    dataOfLine+=item.get(i).getItem().get(j).getNumber()+","+item.get(i).getItem().get(j).getItemName()
+                      +","+item.get(i).getItem().get(j).getItemPrice()
+                      +","+item.get(i).getItem().get(j).getCount()+'\n';
             }
 
-        } else {
+            }
+            else
+            {
+               dataOfHeader+=item.get(i).getNumber()+","+item.get(i).getDate()+","+item.get(i).getCustomer();
+               for (int j = 0; j < item.get(i).getItem().size(); j++) {
+                if(item.get(i).getItem().size()-1!=j)
+              dataOfLine+=item.get(i).getItem().get(j).getNumber()+","+item.get(i).getItem().get(j).getItemName()
+                      +","+item.get(i).getItem().get(j).getItemPrice()
+                      +","+item.get(i).getItem().get(j).getCount()+'\n';
+                else
+                    dataOfLine+=item.get(i).getItem().get(j).getNumber()+","+item.get(i).getItem().get(j).getItemName()
+                      +","+item.get(i).getItem().get(j).getItemPrice()
+                      +","+item.get(i).getItem().get(j).getCount();
+            }
+
+            }
+
         }
+        bufferHeaderFile.write(dataOfHeader);
+        bufferLineFile.write(dataOfLine);
+        bufferHeaderFile.close();
+        bufferLineFile.close();
     }
+    
 }
 class main{
-    public static void main(String args[])  //static method  
-    {  
-        System.out.println("Static method"); 
-        FileOperations f = null ; 
-        f.readFile();
-    }  
+    public static void main(String[]arg)
+    {
+        FileOperations file = null;
+        ArrayList<InvoiceHeader> h = file.readFile();
+        for(int i = 0;i<h.size();i++)
+            {
+                System.out.println("Invoice "+h.get(i).getNumber()+"\n" +
+                        "{\n"+ "  "+h.get(i).getDate()+", "
+                        +h.get(i).getCustomer());
+
+                for(int j = 0;j<h.get(i).getItem().size();j++)
+                {
+                    System.out.println("Invoice"+h.get(i).getItem().get(j).getItemName()+", "
+                            +h.get(i).getItem().get(j).getItemPrice() +", "
+                            +h.get(i).getItem().get(j).getCount());
+                }
+
+                System.out.println("}\n");
+
+
+            }
+        
+    }
 }
